fa4b9731eb3b0766253c35f637a3097738208937
Vasiliev Evgenij <vasiliev.evgenij@gmail.com>
2019-05-09 07:44:34 +0300

    init_commit

---
+++ src/main/java/com/socialserv/SocialservApplication.java
@@ -0,0 +1,13 @@
+package com.socialserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialserv/SocialservApplicationTests.java
@@ -0,0 +1,16 @@
+package com.socialserv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialservApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
